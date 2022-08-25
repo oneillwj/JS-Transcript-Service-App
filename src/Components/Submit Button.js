9b3e8acfcc70e875a8c28c1493384d1a88c46d14
oneillwj <jasmine.w.oneill@gmail.com>
2022-08-25 13:05:31 -0700

    added axios and onclick button

--- src/Components/Submit Button.js
+++ src/Components/Submit Button.js
@@ -1,12 +1,16 @@
 import * as React from 'react';
 import { Button } from '@mui/material';
 import Grid from '@mui/material/Grid';
+import axios from 'axios';
+
+
+
+
 
 export default function SubmitButton() {
   return (
     <Grid container justifyContent="center">
         <Button
-            
             size="medium"
             variant="contained"
             color="primary"
@@ -15,9 +19,9 @@ export default function SubmitButton() {
                 flexDirection: "row",
                 justifyContent:"center",
             }}
-    
-        >
-        Here We Go
+            onClick={() => {console.log("Submit Button has been hit");}} 
+            >
+          Here We Go
         </Button>
     </Grid>
     
